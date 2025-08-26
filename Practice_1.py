0e6157d629e0a6227955fe5cf7fc759729698288
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-08-26 23:03:34 +0900

    1Q

---
+++ Practice_1.py
@@ -0,0 +1,14 @@
+ages = [25 , 32 , 41 , 29 , 38 , 22 , 30]
+
+# 平均を出す関数
+def average(numbers):
+    return sum(numbers) / len(numbers)
+
+# 30歳以上を抽出する関数
+def filter_over_30(numbers):
+    return [age for age in numbers if age >= 30]
+
+
+print("社員データ：", ages)
+print("平均年齢", average(ages))
+print("30歳以上:", filter_over_30(ages))
