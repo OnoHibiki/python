4744abc94c4a2f2883c58de4568780f2c835c434
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-09-01 20:29:00 +0900

    こんばんは

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def output(user_input):
-    out = user_input
-    return out
+#FizzBuzzあぷり
+try:
+    suuzi = int(input("数字を入力して"))
+except ValueError:
+    print("エラー:数字を入力してください")
+    exit()
 
 
-print(output(input("何か入力してください")))
+for i in range(1,suuzi + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
